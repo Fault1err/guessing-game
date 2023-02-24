4f285b581476fc59e2349ede65c7746e2f0c0769
Fault1err <f4ult1err@gmail.com>
2023-02-24 20:44:58 +0300

    added solution

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,26 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        this.guessing;
+        this.min;
+        this.max;
+    }
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
-
+        this.guessing = Math.round((this.min + this.max) / 2);
+        return this.guessing; 
     }
 
     lower() {
-
+        this.max = this.guessing;
     }
 
     greater() {
-
+this.min = this.guessing;
     }
 }
 
